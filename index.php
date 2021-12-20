bedc875a552ed9e9b8210ddd19cf1367eac8531f
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-12-20 17:21:14 +0100

    iinizati es 2 e 4

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ Olimpia Milano - Cantù | 55-60 -->
     ];
  
 
+
     for ($i=0; $i < sizeof($arrMatch); $i++)
     echo $arrMatch[$i]['squadracasa'].' - '. $arrMatch[$i]['squadraospite'].'  |  '.$arrMatch[$i]['punticasa'].' - '. $arrMatch[$i]['puntiospite'].'<br>';
  
@@ -47,12 +48,22 @@ Olimpia Milano - Cantù | 55-60 -->
 ?>
 
 <h2>Snack 2</h2>
-
 <!-- ## Snack 2
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
+<?php
 
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
 
+    if (strlen($name) > 3 && (str_contains($mail, '@')) && is_int($age)) {
+    echo $name.' Accesso riuscito'; 
+    }
+    else {
+        echo 'Accesso negato';
+    }   
 
+?>
 
 
 <h2>Snack 3</h2>
@@ -61,14 +72,76 @@ Passare come parametri GET name, mail e age e verificare (cercando i metodi che 
 Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 Qui l'array di esempio: https://www.codepile.net/pile/R2K5d68z -->
 
+<?php
 
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+    for ($i=0; $i < sizeof($posts); $i++)
+        echo $posts[$i].
+             $posts[$i]['title'].' - '. '<br>'.
+             $posts[$i]['author'].' - '.'<br>'.
+             $posts[$i]['text'].' - '.'<br>';
+
+    ?>
 
 
 
 <h2>Snack 4</h2>
-
 <!-- ## Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta -->
+
+
+<?php
+    $iterazioni=15;
+    $min=1;
+    $max=20;
+
+    for($i=0; $i<$iterazioni; $i++)
+    echo rand($min,$max)." - ";
+
+
+?>
+
+
+
 
 <style>
     *{
